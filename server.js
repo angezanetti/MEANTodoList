a0fc4d51ee05bbf7fc8adb5175b83eb099668022
Xavier Coiffard <xavier.coiffard@sellsecure.com>
2018-05-04 11:05:21 -0700

    add the route for the NG app

--- server.js
+++ server.js
@@ -68,3 +68,8 @@ app.delete('/api/todos/:todo_id', function(req, res) {
 	});
   });
 });
+
+ // Angular application -------------------------------------------------------------
+app.get('*', function(req, res) {
+  res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
