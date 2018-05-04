6693b3033e69b0a90add69c3edc647e69cf1de8b
Xavier Coiffard <xavier.coiffard@sellsecure.com>
2018-05-04 14:22:37 -0700

    add a new task with prompt

--- public/core.js
+++ public/core.js
@@ -1,5 +1,5 @@
 angular.module('todoApp', [])
-  .controller('TodoListController', function($http, $scope) {
+  .controller('TodoListController', function($http, $scope, $window) {
     var todoList = this;
 	  $http.get('/api/todos')
 		.then(function(data) {
@@ -9,21 +9,21 @@ angular.module('todoApp', [])
 		});
  
     todoList.addTodo = function() {
-	  $http.post('/api/todos', {text:todoList.todoText, done:false})
-      todoList.todos.push({text:todoList.todoText, done:false});
+	  var task = $window.prompt('Enter your new task');
+	  $http.post('/api/todos', {text:task, done:false})
+      todoList.todos.push({text:task, done:false});
       todoList.todoText = '';
     };
 
+	// Change the state of a todo 
 	$scope.toggleSelection = function toggleSelection(event, index) {
 	  todo_id = todoList.todos[index]._id;
 	  $http.post('/api/todos/' + todo_id, { done: event.target.checked})
 	};
 
     $scope.delete = function(index) {
-	  console.log('delete' + index);
 	  todo_id = todoList.todos[index]._id;
 	  $http.delete('/api/todos/' + todo_id);
       todoList.todos = todoList.todos.slice(index);
-	  console.log(todoList.todos);
     };
   });
